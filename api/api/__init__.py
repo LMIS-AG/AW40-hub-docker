ae25178bfb6058100911a9d751e7dc74d24a568a
sply88 <sebastian.pauly@tu-dortmund.de>
2023-03-03 10:14:23 +0100

    Update __init__.py
    Beanie is initialized

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,7 +1,22 @@
+from beanie import init_beanie
 from fastapi import FastAPI
+from motor import motor_asyncio
 
+from .data_management import Case, Vehicle, Customer, Workshop
+from .settings import settings
 from .v1 import api_v1
 
 app = FastAPI()
 
 app.mount("/v1", api_v1)
+
+
+@app.on_event("startup")
+async def init_mongo():
+
+    # initialize beanie
+    client = motor_asyncio.AsyncIOMotorClient(settings.mongo_uri)
+    await init_beanie(
+        client[settings.mongo_db],
+        document_models=[Case, Vehicle, Customer, Workshop]
+    )
