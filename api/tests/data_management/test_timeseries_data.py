553d00dab1e25eb74cc3ca3e90b6c66436c6fa6d
sply88 <sebastian.pauly@tu-dortmund.de>
2023-03-15 14:36:06 +0100

    Update test_timeseries_data.py
    Basic coverage of the remaining classes

--- api/tests/data_management/test_timeseries_data.py
+++ api/tests/data_management/test_timeseries_data.py
@@ -1,6 +1,12 @@
+from typing import List
+
 import pytest
-from api.data_management.timeseries_data import GridFSSignalStore
+from api.data_management.timeseries_data import (
+    BaseSignalStore, GridFSSignalStore, NewTimeseriesData, TimeseriesData
+)
+from beanie import PydanticObjectId
 from motor import motor_asyncio
+from pydantic import ValidationError
 
 
 @pytest.fixture
@@ -25,3 +31,115 @@ class TestGridFSSignalStore:
         signal_id = await signal_store.create(signal)
         retrieved_signal = await signal_store.get(signal_id)
         assert signal == retrieved_signal
+
+
+class MockSignalStore(BaseSignalStore):
+    """
+    Mock implementation of the BaseSignalStore interface used for testing
+    subclasses of BaseTimeseriesData.
+    """
+
+    def __init__(self):
+        self.store = {}
+
+    async def create(self, signal: List[float]) -> str:
+        new_id = PydanticObjectId()
+        self.store[new_id] = signal
+        return str(new_id)
+
+    async def get(self, id: str) -> List[float]:
+        return self.store[id]
+
+
+@pytest.fixture
+def timeseries_meta_data():
+    """Valid timeseries meta data"""
+    return {
+        "component": "Batterie",
+        "label": "keine Angabe",
+        "sampling_rate": 1,
+        "duration": 3,
+        "type": "oscillogram"
+    }
+
+
+@pytest.fixture
+def timeseries_signal_id():
+    """Valid timeseries signal id, e.g. needs to work with PydanticObjectId"""
+    return "5eb7cf5a86d9755df3a6c593"
+
+
+@pytest.fixture
+def timeseries_data(timeseries_meta_data, timeseries_signal_id):
+    """Data expected to validate successfully with TimeseriesData"""
+    timeseries_meta_data["signal_id"] = timeseries_signal_id
+    return timeseries_meta_data
+
+
+@pytest.fixture
+def timeseries_signal():
+    return [-1.0, 0, 1.0]
+
+
+@pytest.fixture
+def new_timeseries_data(timeseries_meta_data, timeseries_signal):
+    """Data expected to validate successfully with NewTimeseriesData"""
+    timeseries_meta_data["signal"] = timeseries_signal
+    return timeseries_meta_data
+
+
+class TestTimeseriesData:
+
+    def test_validation_fails_without_signal_id(self, timeseries_meta_data):
+        with pytest.raises(ValidationError):
+            TimeseriesData(**timeseries_meta_data)
+
+    def test_validation_succeeds_with_signal_id(self, timeseries_data):
+        TimeseriesData(**timeseries_data)
+
+    @pytest.mark.asyncio
+    async def test_signal(self, timeseries_signal, timeseries_meta_data):
+        # init a MockSignalStore and manually add a signal
+        signal_store = MockSignalStore()
+        signal_id = await signal_store.create(timeseries_signal)
+
+        # configure TimeseriesData class to use the mock store
+        TimeseriesData.signal_store = signal_store
+
+        # after adding signal_id to metadata, a TimeseriesData instance can
+        # be created
+        timeseries_meta_data["signal_id"] = signal_id
+        timeseries_data = TimeseriesData(**timeseries_meta_data)
+
+        # retrieve the original signal from the TimeseriesData instance
+        retrieved_signal = await timeseries_data.signal
+        assert retrieved_signal == timeseries_signal
+
+
+class TestNewTimeseriesData:
+
+    def test_validation_fails_without_signal(self, timeseries_meta_data):
+        with pytest.raises(ValidationError):
+            NewTimeseriesData(**timeseries_meta_data)
+
+    def test_validation_succeeds_with_signal(self, new_timeseries_data):
+        NewTimeseriesData(**new_timeseries_data)
+
+    @pytest.mark.asyncio
+    async def test_to_timeseries_data(self, new_timeseries_data):
+        # configure class to use MockSignalStore
+        signal_store = MockSignalStore()
+        NewTimeseriesData.signal_store = signal_store
+
+        # convert valid data to model instance
+        new_timeseries_data = NewTimeseriesData(**new_timeseries_data)
+
+        # function under test should return an instance of TimeseriesData
+        timeseries_data = await new_timeseries_data.to_timeseries_data()
+        assert isinstance(timeseries_data, TimeseriesData)
+
+        # there should be single id in store that is reference from the newly
+        # created TimeseriesData instance
+        ids_in_store = list(signal_store.store.keys())
+        assert len(ids_in_store) == 1
+        assert timeseries_data.signal_id == ids_in_store[0]
