7349780b7a1f0d3993dca183002a29f5c28ed532
sply88 <sebastian.pauly@tu-dortmund.de>
2023-04-04 11:03:27 +0200

    Update shared.py
    Sketch of shared routes

--- api/api/routers/shared.py
+++ api/api/routers/shared.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from ..data_management import Case, Customer, Vehicle, Workshop
 
@@ -34,52 +34,69 @@ async def list_cases(
 @router.get("/cases/{case_id}", status_code=200, response_model=Case)
 async def get_case(case_id: str) -> Case:
     case = await Case.get(case_id)
-    return case
+    if case is not None:
+        return case
+    else:
+        exception_detail = f"No case with id `{case_id}`"
+        raise HTTPException(status_code=404, detail=exception_detail)
 
 
-@router.get("/customers")
-async def list_customers(
-        vin: str = None, workshop_id: str = None
-) -> List[Customer]:
+@router.get("/customers", status_code=200, response_model=List[Customer])
+async def list_customers() -> List[Customer]:
     """
-    List all customers in Hub. Query params can be used to filter by `vin` and
-    `workshop_id`.
+    List all customers in Hub.
     """
-    pass
+    customers = await Customer.find_all().to_list()
+    return customers
 
 
-@router.get("/customers/{customer_id}")
+@router.get(
+    "/customers/{customer_id}", status_code=200, response_model=Customer
+)
 async def get_customer(customer_id: str) -> Customer:
-    pass
+    customer = await Customer.get(customer_id)
+    if customer is not None:
+        return customer
+    else:
+        exception_detail = f"No customer with id `{customer_id}`"
+        raise HTTPException(status_code=404, detail=exception_detail)
 
 
-@router.get("/vehicles")
-async def list_vehicles(
-        customer_id: str = None, workshop_id: str = None
-) -> List[Vehicle]:
+@router.get("/vehicles", status_code=200, response_model=List[Vehicle])
+async def list_vehicles() -> List[Vehicle]:
     """
-    List all vehicles in Hub. Query params can be used to filter by
-    `customer_id` and `workshop_id`.
+    List all vehicles in Hub.
     """
-    pass
+    vehicles = await Vehicle.find_all().to_list()
+    return vehicles
 
 
-@router.get("/vehicles/{vin}")
+@router.get("/vehicles/{vin}", status_code=200, response_model=Vehicle)
 async def get_vehicle(vin: str) -> Vehicle:
-    pass
+    vehicle = await Vehicle.find_one({"vin": vin})
+    if vehicle is not None:
+        return vehicle
+    else:
+        exception_detail = f"No vehicle with vin `{vin}`"
+        raise HTTPException(status_code=404, detail=exception_detail)
 
 
-@router.get("/workshops")
-async def list_workshops(
-        customer_id: str = None, vin: str = None
-) -> List[Workshop]:
+@router.get("/workshops", status_code=200, response_model=List[Workshop])
+async def list_workshops() -> List[Workshop]:
     """
-    Get all workshops in Hub. Query params can be used to filter by
-    `customer_id` and `vin`.
+    Get all workshops in Hub.
     """
-    pass
+    workshops = await Workshop.find_all().to_list()
+    return workshops
 
 
-@router.get("/workshops/{workshop_id}")
+@router.get(
+    "/workshops/{workshop_id}", status_code=200, response_model=Workshop
+)
 async def get_workshop(workshop_id: str) -> Workshop:
-    pass
+    workshop = await Workshop.get(workshop_id)
+    if workshop is not None:
+        return workshop
+    else:
+        exception_detail = f"No workshop with id `{workshop_id}`"
+        raise HTTPException(status_code=404, detail=exception_detail)
