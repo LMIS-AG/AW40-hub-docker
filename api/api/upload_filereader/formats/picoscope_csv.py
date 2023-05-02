3f40f4e8a75f4e8d5e9957a90fa33ef4c7b11379
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2023-05-02 18:39:30 +0200

    Edited CSV Parser: Added support for semicolon delimiter Added support for german and english files

--- api/api/upload_filereader/formats/picoscope_csv.py
+++ api/api/upload_filereader/formats/picoscope_csv.py
@@ -14,17 +14,32 @@ conv_table = {
     "(μV)": 1e-6
 }
 
-HEADER_CHECK = re.compile(r"^Time(?:,Channel [A-Z])+$")
+#channel_names = [
+#    "Kanal",
+#    "Channel"
+#]
+
+time_names = [
+    "Zeit",
+    "Time"
+]
+
+#HEADER_CHECK = re.compile(r"^(?:Time|Zeit)(?:,(?:Channel|Kanal) [A-Z])+$")
+HEADER_CHECK = re.compile(
+    r"^\w*(?P<delimiter>[,;])(?:\w* [A-Z])(?:\1\w* [A-Z])*$")
 CONVHEADER_CHECK = re.compile(
-    r"^\((?:m|μ){0,1}(?:s|V)\)(?:,\((?:m|μ){0,1}(?:s|V)\))+$")
+    r"^\((?:m|μ){0,1}(?:s|V)\)(?:[,;]\((?:m|μ){0,1}(?:s|V)\))+$")
+ALLOWED_CHANNEL = re.compile(r"[A-Z]")
+CHANNEL_CHECK = re.compile(r"^(?P<channel_name>\w*) (?P<channel>[A-Z])$")
 
 
 class PicoscopeCSVReader(FileReader):
     def read_file(self, file: BinaryIO) -> List[dict]:
         result = []
-        if not self.__probe(file):
+        validated, delimiter =  self.__probe(file)
+        if not validated:
             raise Exception("conversion failed: wrong format")
-        data = self.__csv_to_dict(file)
+        data = self.__csv_to_dict(file, delimiter)
         duration = self.__calculate_duration(data)
         sampling_rate = self.__calculate_sampling_rate(data)[0]
         for key in data.keys():
@@ -39,19 +54,39 @@ class PicoscopeCSVReader(FileReader):
                     }
                 })
         return result
+    
+    def __translate_header(self, header):
+        translated = []
+        for item in header:
+            if item in time_names:
+                translated.append("Time")
+            else:
+                channel_check = CHANNEL_CHECK.match(item)
+                if channel_check:
+                    channel = channel_check["channel"]
+                    translated.append("Channel {}".format(channel))
+        if len(translated) != len(header):
+            raise Exception("conversion failed: failed to translate header")
+        return translated
 
     def __probe(self, file):
+        delimiter = ""
+        validated = False
         file_iter = codecs.iterdecode(file, "utf-8")
         header = next(file_iter).strip()
         conv_header = next(file_iter).strip()
-        validated = HEADER_CHECK.match(header) and CONVHEADER_CHECK.\
-            match(conv_header)
+        header_check = HEADER_CHECK.match(header)
+        conv_header_check = CONVHEADER_CHECK.match(conv_header)
+        if header_check and conv_header_check:
+            delimiter = header_check["delimiter"]
+            validated = True
         file.seek(0)
-        return validated
+        return validated, delimiter
 
-    def __csv_to_dict(self, file):
-        reader = csv.reader(codecs.iterdecode(file, 'utf-8'))
-        header = next(reader)
+    def __csv_to_dict(self, file, delimiter):
+        reader = csv.reader(codecs.iterdecode(file, 'utf-8'),
+                            delimiter=delimiter)
+        header = self.__translate_header(next(reader))
         data = {}
         conversion = []
 
