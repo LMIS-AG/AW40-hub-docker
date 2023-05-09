066c5d7a25c8e7749a78ed36f93bc1823d7a5538
julian-marco <julian.ende@lmis.de>
2023-05-09 14:05:24 +0200

    adjust env variables

--- keycloak/keycloak-config.sh
+++ keycloak/keycloak-config.sh
@@ -2,7 +2,7 @@
 
 # Authenticate in order to use the Keycloak Admin CLI
 /opt/keycloak/bin/kcadm.sh config credentials \
-    --server http://${IP_ADDRESS}:8080 \
+    --server http://${KEYCLOAK_HOST_IP_ADDRESS}:8080 \
     --realm master \
     --user ${KEYCLOAK_ADMIN} \
     --password ${KEYCLOAK_ADMIN_PASSWORD}
@@ -13,10 +13,10 @@
 # Create first user
 /opt/keycloak/bin/kcadm.sh create users \
     -r werkstatt-hub \
-    -s username=${KC_ADMIN_WERKSTATTHUB} \
+    -s username=${MINIO_ADMIN_WERKSTATTHUB} \
     -s enabled=true \
     -s attributes.policy=consoleAdmin \
-    -s credentials='[{"type":"password","value":"'${KC_ADMIN_WERKSTATTHUB_PASSWORD}'"}]'
+    -s credentials='[{"type":"password","value":"'${MINIO_ADMIN_WERKSTATTHUB_PASSWORD}'"}]'
 
 # Get the ID of the 'minio' client in the 'werkstatt-hub' realm
 CLIENT_ID=$(/opt/keycloak/bin/kcadm.sh get clients -r werkstatt-hub -q clientId=minio -F id | grep -oP '\w{8}-(\w{4}-){3}\w{12}' | cut -f1)
--- minio/minio-config.sh
+++ minio/minio-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/bin/mc alias set myminio http://${IP_ADDRESS}:9000 ${S3_ROOT_USER} ${S3_ROOT_PASSWORD};
+/usr/bin/mc alias set myminio http://${KEYCLOAK_HOST_IP_ADDRESS}:9000 ${S3_ROOT_USER} ${S3_ROOT_PASSWORD};
 /usr/bin/mc mb myminio/werkstatthub;
 exit 0;
-
