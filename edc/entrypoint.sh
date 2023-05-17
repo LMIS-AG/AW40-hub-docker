84ca93c44ce6966ccfd4a0dbeb68a7f3b04e81d1
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2023-05-17 17:21:50 +0200

    Updated EDC Docker Build

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -2,3 +2,4 @@
 
 # Replace the placeholder in the properties file with the value of the environment variable
 sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" config.properties
+java -Dedc.fs.config=config.properties -Daws.accessKeyId=${S3_ROOT_USER} -Daws.secretAccessKey=${S3_ROOT_PASSWORD} -jar provider.jar
