ae5340c3cbc8e6191c108825f392b04cb81dcaad
sply88 <sebastian.pauly@tu-dortmund.de>
2023-05-26 12:54:59 +0200

    Update v1.py
    Diagnostics router included in v1 api

--- api/api/v1.py
+++ api/api/v1.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from .routers import health, shared, workshop
+from .routers import health, shared, workshop, diagnostics
 
 all_tags_metadata = [
     *health.tags_metadata,
@@ -17,3 +17,4 @@ api_v1 = FastAPI(
 api_v1.include_router(health.router, prefix="/health")
 api_v1.include_router(shared.router, prefix="/shared")
 api_v1.include_router(workshop.router)
+api_v1.include_router(diagnostics.router, prefix="/diagnostics")
