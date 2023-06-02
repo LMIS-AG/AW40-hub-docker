d8090838c38d03902f7b25ba123e436ff65251a5
Timo Thurow <timo.thurow@hs-osnabrueck.de>
2023-06-02 19:22:43 +0200

    Made gradlew executable and switched to temurin

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Replace the placeholder in the properties file with the value of the environment variable
 sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" config.properties
