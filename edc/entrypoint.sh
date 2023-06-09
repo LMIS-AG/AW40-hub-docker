2cd16ec33ab731fd0b7142c31e7459471d9f6b98
julian-marco <julian.ende@lmis.de>
2023-06-09 14:58:58 +0200

    further simplify the connector setup

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -5,4 +5,4 @@ cp /config/config.properties config.properties
 
 # Replace the placeholder in the copied properties file with the value of the environment variable
 sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" config.properties
-java -Daws.accessKeyId=${S3_ROOT_USER} -Daws.secretAccessKey=${S3_ROOT_PASSWORD} -jar connector.jar
+java -jar connector.jar
