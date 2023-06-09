4557966bd5fdf9f9a87dd60eb02bae6a2780f06d
julian-marco <julian.ende@lmis.de>
2023-06-09 13:35:08 +0200

    inject config.properties into container

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# Replace the placeholder in the properties file with the value of the environment variable
+# Make a copy of the mounted file
+cp /config/config.properties config.properties
+
+# Replace the placeholder in the copied properties file with the value of the environment variable
 sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" config.properties
-java -Dedc.fs.config=config.properties -Daws.accessKeyId=${S3_ROOT_USER} -Daws.secretAccessKey=${S3_ROOT_PASSWORD} -jar connector.jar
+java -Daws.accessKeyId=${S3_ROOT_USER} -Daws.secretAccessKey=${S3_ROOT_PASSWORD} -jar connector.jar
