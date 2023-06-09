eeff8341831257ac494ed02758b0a4344be13775
julian-marco <julian.ende@lmis.de>
2023-06-09 16:02:06 +0200

    make edc config interchangeable via dev.env

--- edc/entrypoint.sh
+++ edc/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Make a copy of the mounted file
-cp /config/config.properties config.properties
+cp /configs/$EDC_FS_CONFIG $EDC_FS_CONFIG
 
 # Replace the placeholder in the copied properties file with the value of the environment variable
-sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" config.properties
+sed -i "s/\${HOST_IP_ADDRESS}/$HOST_IP_ADDRESS/g" $EDC_FS_CONFIG
 java -jar connector.jar
