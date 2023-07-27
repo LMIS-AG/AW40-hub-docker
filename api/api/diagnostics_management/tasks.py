22551668ccf76f6a63effd6e40849f63e8df5b4c
sply88 <sebastian.pauly@tu-dortmund.de>
2023-07-27 12:56:31 +0200

    Update task.py and test_tasks.py
    Simplification possible due to polling in diagnostics service

--- api/api/diagnostics_management/tasks.py
+++ api/api/diagnostics_management/tasks.py
@@ -1,12 +1,9 @@
 from celery import Celery
 
-from ..data_management import DiagnosisStatus, DiagnosisDB
-
 
 class DiagnosticTaskManager:
     """
-    Manages handing over tasks to the diagnostics service, depending on the
-    state of the current diagnosis state in the database
+    Manages handing over tasks to the diagnostics service.
     """
 
     _celery: Celery = None
@@ -21,18 +18,7 @@ class DiagnosticTaskManager:
         cls._celery = celery
 
     async def __call__(self, diagnosis_id):
-        if diagnosis_id:
-            diag = await DiagnosisDB.get(diagnosis_id)
-            if diag.status is None or \
-                    diag.status == DiagnosisStatus.action_required:
-                # Last status indicates a fresh diagnosis or that a user action
-                # was required. Change status to processing and hand the
-                # diagnosis over to the diagnostics service for consideration.
-                diag.status = DiagnosisStatus.processing
-                await diag.save()
-                self._celery.send_task(
-                    self._diagnostic_task_name, (str(diagnosis_id),)
-                )
-
-            # In other cases (diagnosis is already 'processing' or 'finished'),
-            # there is no need to inform the diagnostics service
+        """Send a diagnosis id to the diagnostics backend for processing."""
+        self._celery.send_task(
+            self._diagnostic_task_name, (str(diagnosis_id),)
+        )
--- api/tests/diagnostics_management/test_tasks.py
+++ api/tests/diagnostics_management/test_tasks.py
@@ -1,22 +1,14 @@
 import pytest
-
-from celery import Celery
-from bson import ObjectId
-
 from api.diagnostics_management import tasks
-from api.data_management import DiagnosisDB
+from bson import ObjectId
+from celery import Celery
 
 
 @pytest.fixture
 def DiagnosticTaskManager():
     yield tasks.DiagnosticTaskManager
+    # unset celery configuration after each test
     tasks.DiagnosticTaskManager.set_celery(None)
-
-
-@pytest.fixture
-def manage_diagnostic_task(DiagnosticTaskManager):
-    DiagnosticTaskManager.set_celery(Celery())
-    yield DiagnosticTaskManager()
 
 
 class TestDiagnosticTaskManager:
@@ -30,41 +22,12 @@ class TestDiagnosticTaskManager:
             DiagnosticTaskManager()
 
     @pytest.mark.asyncio
-    async def test_call_no_diagnosis(self, manage_diagnostic_task):
-        manage_diagnostic_task(None)
-
-    @pytest.mark.asyncio
-    @pytest.mark.parametrize("status", ["processing", "finished"])
-    async def test_call_does_not_send_task(
+    async def test_call(
             self,
-            status,
-            manage_diagnostic_task,
-            initialized_beanie_context,
+            DiagnosticTaskManager,
             monkeypatch
     ):
-        def mock_send_task(*args, **kwargs):
-            raise Exception("This function should not be called")
-
-        monkeypatch.setattr(Celery, "send_task", mock_send_task)
-
-        async with initialized_beanie_context:
-            diag = await DiagnosisDB(
-                **{"case_id": ObjectId(), "status": status}
-            ).create()
-
-            await manage_diagnostic_task(diag.id)
-
-            # status should be unchanged
-            diag = await DiagnosisDB.get(diag.id)
-            assert diag.status == status
-
-    @pytest.mark.asyncio
-    async def test_call_does_send_task_for_status_action_required(
-            self,
-            manage_diagnostic_task,
-            initialized_beanie_context,
-            monkeypatch
-    ):
+        DiagnosticTaskManager.set_celery(Celery())
         send_task_args = []
 
         def mock_send_task(self, name, args):
@@ -72,61 +35,11 @@ class TestDiagnosticTaskManager:
             send_task_args.append(args)
 
         monkeypatch.setattr(Celery, "send_task", mock_send_task)
+
         diag_id = ObjectId()
+        await DiagnosticTaskManager()(diag_id)
 
-        async with initialized_beanie_context:
-            # create diagnosis with status action_required
-            diag = await DiagnosisDB(
-                **{
-                    "id": diag_id,
-                    "case_id": ObjectId(),
-                    "status": "action_required"
-                }
-            ).create()
-
-            await manage_diagnostic_task(diag.id)
-
-            # status should be 'processing' now
-            diag = await DiagnosisDB.get(diag_id)
-            assert diag.status == "processing"
-
-            # confirm expected call to Celery.send_task
-            assert send_task_args[0] == \
-                   manage_diagnostic_task._diagnostic_task_name
-            assert send_task_args[1][0] == str(diag_id)
-
-    @pytest.mark.asyncio
-    async def test_call_does_send_task_for_new_diag(
-            self,
-            manage_diagnostic_task,
-            initialized_beanie_context,
-            monkeypatch
-    ):
-        send_task_args = []
-
-        def mock_send_task(self, name, args):
-            send_task_args.append(name)
-            send_task_args.append(args)
-
-        monkeypatch.setattr(Celery, "send_task", mock_send_task)
-        diag_id = ObjectId()
-
-        async with initialized_beanie_context:
-            # Create "new" diagnosis, e.g. one without a status
-            diag = await DiagnosisDB(
-                **{
-                    "id": diag_id,
-                    "case_id": ObjectId()
-                }
-            ).create()
-
-            await manage_diagnostic_task(diag.id)
-
-            # status should be 'processing' now
-            diag = await DiagnosisDB.get(diag_id)
-            assert diag.status == "processing"
-
-            # confirm expected call to Celery.send_task
-            assert send_task_args[0] == \
-                   manage_diagnostic_task._diagnostic_task_name
-            assert send_task_args[1][0] == str(diag_id)
+        # confirm expected call to Celery.send_task
+        assert send_task_args[0] == \
+               DiagnosticTaskManager._diagnostic_task_name
+        assert send_task_args[1] == (str(diag_id),)
