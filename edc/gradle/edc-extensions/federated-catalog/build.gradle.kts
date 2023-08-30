841b0385acd4a85a27a8f5a33053a45d33940e41
julian-marco <julian.ende@lmis.de>
2023-08-30 13:41:01 +0200

    groupId -> edcGroupId in federated catalog build gradle

--- edc/gradle/edc-extensions/federated-catalog/build.gradle.kts
+++ edc/gradle/edc-extensions/federated-catalog/build.gradle.kts
@@ -5,20 +5,20 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-val groupId: String by project
+val edcGroupId: String by project
 val edcVersion: String by project
 
 dependencies {
-    implementation("$groupId:core-spi:$edcVersion")
-    implementation("$groupId:web-spi:$edcVersion")
-    implementation("$groupId:federated-catalog-spi:$edcVersion")
-    implementation("$groupId:federated-catalog-core:$edcVersion")
-    implementation("$groupId:management-api-configuration:$edcVersion")
-    implementation("$groupId:json-ld:$edcVersion")
-    implementation("$groupId:json-ld-spi:$edcVersion")
-    implementation("$groupId:connector-core:$edcVersion")
+    implementation("$edcGroupId:core-spi:$edcVersion")
+    implementation("$edcGroupId:web-spi:$edcVersion")
+    implementation("$edcGroupId:federated-catalog-spi:$edcVersion")
+    implementation("$edcGroupId:federated-catalog-core:$edcVersion")
+    implementation("$edcGroupId:management-api-configuration:$edcVersion")
+    implementation("$edcGroupId:json-ld:$edcVersion")
+    implementation("$edcGroupId:json-ld-spi:$edcVersion")
+    implementation("$edcGroupId:connector-core:$edcVersion")
 
-    implementation("$groupId:runtime-metamodel:$edcVersion")
+    implementation("$edcGroupId:runtime-metamodel:$edcVersion")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.15")
 
 }
