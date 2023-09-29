683a1d745b57992e2f4982e437be6407a2eade55
Marcel Ruland <info@marcelruland.com>
2023-09-29 14:27:34 +0200

    Format

--- api/api/middleware/sts_middleware.py
+++ api/api/middleware/sts_middleware.py
@@ -4,6 +4,6 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class STSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+        response.headers["Strict-Transport-Security"] = \
+            "max-age=31536000; includeSubDomains"
         return response
-
