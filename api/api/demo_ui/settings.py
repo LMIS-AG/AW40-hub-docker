f50084f781367823208a4ef366348829f3f5f050
sply88 <sebastian.pauly@tu-dortmund.de>
2023-10-05 09:02:41 +0200

    Update ui.py and Add templates for data management sites
    Full UI support of current diagnostic capabilities

---
+++ api/api/demo_ui/settings.py
@@ -0,0 +1,8 @@
+from pydantic import BaseSettings
+
+
+class Settings:
+    hub_api_base_url: str = "http://127.0.0.1:8000/v1"
+
+
+settings = Settings()
--- api/api/demo_ui/ui.py
+++ api/api/demo_ui/ui.py
@@ -1,47 +1,358 @@
 import httpx
-
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, UploadFile, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from .settings import settings
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="api/demo_ui/static"), name="static")
+
 templates = Jinja2Templates(directory="api/demo_ui/templates")
 
 
-@app.get("/{workshop_id}/cases/{case_id}/diag", response_class=HTMLResponse)
-def diagnosis_report(request: Request, workshop_id: str, case_id: str):
-    """Render an HTML report of a diagnosis."""
-    try:
-        # fetch diagnosis data from the api
-        url = f"http://127.0.0.1:8000/v1/{workshop_id}/cases/{case_id}/diag"
-        response = httpx.get(url)
+@app.exception_handler(httpx.HTTPStatusError)
+async def http_status_error_handler(request, exc):
+    return templates.TemplateResponse(
+        "http_exception.html",
+        {
+            "request": request,
+            "status_code": exc.response.status_code,
+            "details": exc.response.json().get("detail")
+        }
+    )
+
+
+def get_cases_url(workshop_id: str) -> str:
+    return f"{settings.hub_api_base_url}/{workshop_id}/cases"
+
+
+def get_case_url(workshop_id: str, case_id: str) -> str:
+    cases_url = get_cases_url(workshop_id)
+    return f"{cases_url}/{case_id}"
+
+
+def _get_data_url(
+        workshop_id: str, case_id: str, data_type: str, data_id: str = None
+) -> str:
+    case_url = get_case_url(workshop_id, case_id)
+    data_url = f"{case_url}/{data_type}"
+    if data_id is not None:
+        data_url = f"{data_url}/{data_id}"
+    return data_url
+
+
+def get_obd_data_url(
+        workshop_id: str, case_id: str, data_id: str = None
+) -> str:
+    return _get_data_url(workshop_id, case_id, "obd_data", data_id)
+
+
+def get_timeseries_data_url(
+        workshop_id: str, case_id: str, data_id: str = None
+) -> str:
+    return _get_data_url(workshop_id, case_id, "timeseries_data", data_id)
+
+
+def get_symptoms_url(
+        workshop_id: str, case_id: str, data_id: str = None
+) -> str:
+    return _get_data_url(workshop_id, case_id, "symptoms", data_id)
+
+
+def get_diagnosis_url(workshop_id: str, case_id: str) -> str:
+    return _get_data_url(workshop_id, case_id, "diag", None)
+
+
+def get_from_api(url: str) -> dict:
+    response = httpx.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+async def post_to_api(url: str, **kwargs) -> dict:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, **kwargs)
         response.raise_for_status()
-        diag = response.json()
+        return response.json()
 
-        # for each log entry with attachment, replace the attachment id with
-        # full attachment url
-        for log_entry in diag["state_machine_log"]:
-            attachment_id = log_entry["attachment"]
-            if attachment_id is not None:
-                attachment_url = f"{url}/attachments/{attachment_id}"
-                log_entry["attachment"] = attachment_url
 
-        return templates.TemplateResponse(
-            "diagnosis_report.html",
-            {
-                "request": request,
-                "case_id": diag["case_id"],
-                "diag_status": diag["status"],
-                "state_machine_log": diag["state_machine_log"],
-                "todos": diag["todos"]
-            }
+@app.get("/ui", response_class=HTMLResponse)
+def login(request: Request):
+    return templates.TemplateResponse(
+        "login.html", {"request": request}
+    )
+
+
+@app.post("/ui", response_class=RedirectResponse, status_code=303)
+def login(request: Request, workshop_id: str = Form()):
+    redirect_url = app.url_path_for("cases", workshop_id=workshop_id)
+    return redirect_url
+
+
+@app.get("/ui/{workshop_id}/cases", response_class=HTMLResponse)
+def cases(request: Request, ressource_url: str = Depends(get_cases_url)):
+    cases = get_from_api(ressource_url)
+    return templates.TemplateResponse(
+        "cases.html",
+        {
+            "request": request,
+            "cases": cases
+        }
+    )
+
+
+@app.get("/ui/{workshop_id}/cases/new", response_class=HTMLResponse)
+def new_case_get(request: Request):
+    return templates.TemplateResponse(
+        "new_case.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.post(
+    "/ui/{workshop_id}/cases/new",
+    response_class=RedirectResponse, status_code=303
+)
+async def new_case_post(
+        request: Request, ressource_url: str = Depends(get_cases_url)
+):
+    form = await request.form()
+    # remove empty fields
+    form = {k: v for k, v in form.items() if v}
+    case = await post_to_api(ressource_url, json=dict(form))
+    case_id = case["_id"]
+    redirect_url = app.url_path_for(
+        "case", workshop_id=case["workshop_id"], case_id=case["_id"]
+    )
+    return redirect_url
+
+
+@app.get("/ui/{workshop_id}/cases/{case_id}", response_class=HTMLResponse)
+def case(request: Request, ressource_url: str = Depends(get_case_url)):
+    case = get_from_api(ressource_url)
+    if case["diagnosis_id"] is not None:
+        # get diagnosis and embed in case
+        diagnosis = get_from_api(
+            get_diagnosis_url(
+                case["workshop_id"], case["_id"]
+            )
         )
-    except httpx.HTTPStatusError:
-        return templates.TemplateResponse(
-            "http_exception.html",
-            {
-                "request": request,
-                "status_code": response.status_code,
-                "details": response.json().get("detail")
-            }
+        case["diagnosis"] = diagnosis
+
+    return templates.TemplateResponse(
+        "case.html",
+        {
+            "request": request,
+            "case": case
+        }
+    )
+
+
+@app.get(
+    "/ui/{workshop_id}/cases/{case_id}/obd_data/new",
+    response_class=HTMLResponse
+)
+def new_obd_data_get(request: Request):
+    return templates.TemplateResponse(
+        "new_obd_data.html", {"request": request}
+    )
+
+
+@app.post(
+    "/ui/{workshop_id}/cases/{case_id}/obd_data/new",
+    response_class=RedirectResponse,
+    status_code=303
+)
+async def new_obd_data_post(
+        request: Request,
+        ressource_url: str = Depends(get_obd_data_url),
+        dtcs_text: str = Form(default=None),
+        vcds_file: UploadFile = None
+):
+    if dtcs_text:
+        dtcs = dtcs_text.split("\r\n")
+        obd_data = {"dtcs": dtcs}
+        case = await post_to_api(ressource_url, json=obd_data)
+    if vcds_file:
+        ressource_url = f"{ressource_url}/upload/vcds"
+        case = await post_to_api(
+            ressource_url,
+            files={"upload": (vcds_file.filename, vcds_file.file)}
         )
+
+    new_data_id = case["obd_data"][-1]["data_id"]
+    redirect_url = app.url_path_for(
+        "obd_data",
+        workshop_id=case["workshop_id"],
+        case_id=case["_id"],
+        data_id=new_data_id
+    )
+    return redirect_url
+
+
+@app.get(
+    "/ui/{workshop_id}/cases/{case_id}/obd_data/{data_id}",
+    response_class=HTMLResponse
+)
+def obd_data(
+        request: Request,
+        ressource_url: str = Depends(get_obd_data_url),
+        dtc: str = None
+):
+    obd_data = get_from_api(ressource_url)
+    return templates.TemplateResponse(
+        "obd_data.html",
+        {
+            "request": request,
+            "obd_data": obd_data
+        }
+    )
+
+
+@app.get(
+    "/ui/{workshop_id}/cases/{case_id}/timeseries_data/new",
+    response_class=HTMLResponse
+)
+def new_timeseries_data_get(
+        request: Request
+):
+    return templates.TemplateResponse(
+        "new_timeseries_data.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.post(
+    "/ui/{workshop_id}/cases/{case_id}/timeseries_data/new",
+    response_class=RedirectResponse,
+    status_code=303
+)
+async def new_timeseries_data_post(
+        request: Request,
+        ressource_url: str = Depends(get_timeseries_data_url)
+):
+    form = await request.form()
+    form = dict(form)
+    form["file_format"] = "Picoscope CSV"
+    picoscope_file = form.pop("picoscope_file")
+    ressource_url = f"{ressource_url}/upload/picoscope"
+    case = await post_to_api(
+        ressource_url,
+        files={"upload": (picoscope_file.filename, picoscope_file.file)},
+        data=form
+    )
+
+    new_data_id = case["timeseries_data"][-1]["data_id"]
+    redirect_url = app.url_path_for(
+        "timeseries_data",
+        workshop_id=case["workshop_id"],
+        case_id=case["_id"],
+        data_id=new_data_id
+    )
+    return redirect_url
+
+
+@app.get(
+    "/ui/{workshop_id}/cases/{case_id}/timeseries_data/{data_id}",
+    response_class=HTMLResponse
+)
+def timeseries_data(
+        request: Request,
+        ressource_url: str = Depends(get_timeseries_data_url),
+):
+    timeseries_data = get_from_api(ressource_url)
+    signal_url = f"{ressource_url}/signal"
+    signal = get_from_api(signal_url)
+    # convert signal to 2d array with columns 'Zeit' and 'Signal'
+    sr = timeseries_data["sampling_rate"]
+    signal = [[i/sr, v] for i, v in enumerate(signal)]
+
+    return templates.TemplateResponse(
+        "timeseries_data.html",
+        {
+            "request": request,
+            "timeseries_data": timeseries_data,
+            "signal": signal
+        }
+    )
+
+
+@app.get(
+    "/ui/{workshop_id}/cases/{case_id}/symptoms/new",
+    response_class=HTMLResponse
+)
+def new_symptom_get(request: Request):
+    return templates.TemplateResponse(
+        "new_symptom.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.post(
+    "/ui/{workshop_id}/cases/{case_id}/symptoms/new",
+    response_class=RedirectResponse,
+    status_code=303
+)
+async def new_symptom_post(
+        request: Request,
+        ressource_url: str = Depends(get_symptoms_url)
+):
+    form = await request.form()
+    case = await post_to_api(ressource_url, json=dict(form))
+    redirect_url = app.url_path_for(
+        "case", workshop_id=case["workshop_id"], case_id=case["_id"]
+    )
+    return redirect_url
+
+
+@app.post(
+    "/ui/{workshop_id}/cases/{case_id}/diag",
+    response_class=RedirectResponse,
+    status_code=303
+)
+async def start_diagnosis(
+        request: Request,
+        ressource_url: str = Depends(get_diagnosis_url)
+):
+    await post_to_api(ressource_url)
+    redirect_url = app.url_path_for(
+        "case",
+        workshop_id=request.path_params["workshop_id"],
+        case_id=request.path_params["case_id"]
+    )
+    return redirect_url
+
+
+@app.get("/ui/{workshop_id}/cases/{case_id}/diag", response_class=HTMLResponse)
+def diagnosis_report(
+        request: Request, ressource_url: str = Depends(get_diagnosis_url)
+):
+    diag = get_from_api(ressource_url)
+
+    # for each log entry with attachment, replace the attachment id with
+    # full attachment url
+    for log_entry in diag["state_machine_log"]:
+        attachment_id = log_entry["attachment"]
+        if attachment_id is not None:
+            attachment_url = f"{ressource_url}/attachments/{attachment_id}"
+            log_entry["attachment"] = attachment_url
+
+    return templates.TemplateResponse(
+        "diagnosis_report.html",
+        {
+            "request": request,
+            "case_id": diag["case_id"],
+            "diag_status": diag["status"],
+            "state_machine_log": diag["state_machine_log"],
+            "todos": diag["todos"]
+        }
+    )
