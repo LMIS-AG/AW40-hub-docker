a1981d81ecd362f8518f60f3d81eec40504270a6
mgr <mgr@lmis.de>
2023-11-14 13:50:02 +0100

    return mock data for get endpoints

--- frontend/app/lib/providers/diagnosis_provider.dart
+++ frontend/app/lib/providers/diagnosis_provider.dart
@@ -1,4 +1,6 @@
+import "package:aw40_hub_frontend/models/diagnosis_model.dart";
 import "package:aw40_hub_frontend/services/services.dart";
+import "package:aw40_hub_frontend/utils/enums.dart";
 import "package:flutter/material.dart";
 import "package:http/http.dart";
 import "package:logging/logging.dart";
@@ -11,7 +13,42 @@ class DiagnosisProvider with ChangeNotifier {
   final Logger _logger = Logger("diagnosis_provider");
   late String workShopId;
 
-  Future<bool> getDiagnosis(String caseId) async {
+  Future<List<DiagnosisModel>> getDiagnoses(List<String> caseIDs) async {
+    // TODO implement (call getDiagnosis for each caseId)
+
+    // mock data
+    final List<DiagnosisModel> mockData = [
+      DiagnosisModel(
+        id: "1",
+        timestamp: DateTime.now(),
+        status: DiagnosisStatus.scheduled,
+        caseId: "ABC123",
+        stateMachineLog: ["Step 1", "Step 2"],
+        todos: ["Task 1", "Task 2"],
+      ),
+      DiagnosisModel(
+        id: "2",
+        timestamp: DateTime.now(),
+        status: DiagnosisStatus.finished,
+        caseId: "XYZ789",
+        stateMachineLog: ["Step 1", "Step 2", "Step 3"],
+        todos: ["Task 1", "Task 2", "Task 3"],
+      ),
+      DiagnosisModel(
+        id: "3",
+        timestamp: DateTime.now(),
+        status: DiagnosisStatus.processing,
+        caseId: "DEF456",
+        stateMachineLog: ["Step 1"],
+        todos: ["Task 1"],
+      ),
+    ];
+
+    return mockData;
+  }
+
+// TODO adjust return type
+  Future<DiagnosisModel?> getDiagnosis(String caseId) async {
     final Response response =
         await _httpService.getDiagnosis(workShopId, caseId);
     if (response.statusCode != 200) {
@@ -19,7 +56,7 @@ class DiagnosisProvider with ChangeNotifier {
         "Could not get diagnosis. "
         "${response.statusCode}: ${response.reasonPhrase}",
       );
-      return false;
+      return null;
     }
 
     // TODO adjust
@@ -27,7 +64,16 @@ class DiagnosisProvider with ChangeNotifier {
     //final CaseDto receivedCase = CaseDto.fromJson(body);
     //_cases.add(receivedCase.toModel());
     notifyListeners();
-    return true;
+
+    // mock data
+    return DiagnosisModel(
+      id: "2",
+      timestamp: DateTime.now(),
+      status: DiagnosisStatus.finished,
+      caseId: "XYZ789",
+      stateMachineLog: ["Step 1", "Step 2", "Step 3"],
+      todos: ["Task 1", "Task 2", "Task 3"],
+    );
   }
 
   Future<bool> startDiagnosis(String caseId) async {
