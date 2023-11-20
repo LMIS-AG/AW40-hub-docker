e911d27d18ddecef35a7b7cd6078d2acbeb644cb
mgr <mgr@lmis.de>
2023-11-20 13:23:51 +0100

    minor change

--- frontend/app/lib/utils/extensions.dart
+++ frontend/app/lib/utils/extensions.dart
@@ -14,6 +14,7 @@ extension DateExtension on DateTime {
 }
 
 extension DateTimeExtension on DateTime {
+  // ignore: lines_longer_than_80_chars
   String toGermanDateTimeString() =>
       "$day.$month.$year, ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')} Uhr";
 }
