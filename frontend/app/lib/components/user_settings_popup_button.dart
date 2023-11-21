4acaf48ad6b42f0b4b133e1ab000d4179613adce
Marcel Ruland <info@marcelruland.com>
2023-11-21 13:38:45 +0100

    Add theme chooser to UserSettings

--- frontend/app/lib/components/user_settings_popup_button.dart
+++ frontend/app/lib/components/user_settings_popup_button.dart
@@ -16,6 +16,7 @@ class UserSettingsPopupButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return PopupMenuButton<PopupMenuItem>(
       itemBuilder: (context) => [
+        const PopupMenuItem(child: ThemeChooser()),
         const PopupMenuItem(child: LanguageChooser()),
         PopupMenuItem(
           child: const Text("Logout"),
@@ -23,6 +24,36 @@ class UserSettingsPopupButton extends StatelessWidget {
             context,
             listen: false,
           ).logout(),
+        ),
+      ],
+    );
+  }
+}
+
+class ThemeChooser extends StatelessWidget {
+  const ThemeChooser({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final currentThemeMode = Provider.of<ThemeProvider>(context).themeMode;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text("Theme"),
+        SegmentedButton(
+          segments: const <ButtonSegment<ThemeMode>>[
+            ButtonSegment(value: ThemeMode.light, label: Text("Light")),
+            ButtonSegment(value: ThemeMode.dark, label: Text("Dark")),
+          ],
+          selected: {
+            currentThemeMode,
+          },
+          onSelectionChanged: (p0) {
+            Provider.of<ThemeProvider>(context, listen: false)
+                .setThemeMode(p0.first);
+          },
         ),
       ],
     );
