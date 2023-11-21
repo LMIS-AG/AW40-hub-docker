6f49a98535f11bd1175087d755275dc06c1ae0ed
Marcel Ruland <info@marcelruland.com>
2023-11-21 14:15:07 +0100

    Update to Material guidelines

--- frontend/app/lib/views/diagnosis_detail_view.dart
+++ frontend/app/lib/views/diagnosis_detail_view.dart
@@ -102,6 +102,7 @@ class _DesktopDiagnosisDetailView extends State<DesktopDiagnosisDetailView> {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = theme.colorScheme;
 
     // TODO adjust this section in Story 61883
     final List<String> attributes = [
@@ -128,7 +129,10 @@ class _DesktopDiagnosisDetailView extends State<DesktopDiagnosisDetailView> {
                 backgroundColor: const Color.fromARGB(0, 0, 0, 0),
                 title: Text(
                   tr("diagnoses.details.headline"),
-                  style: Theme.of(context).textTheme.displaySmall,
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall
+                      ?.copyWith(color: colorScheme.onPrimaryContainer),
                 ),
                 actions: [
                   IconButton(
@@ -147,8 +151,18 @@ class _DesktopDiagnosisDetailView extends State<DesktopDiagnosisDetailView> {
                   (i) => TableRow(
                     children: [
                       const SizedBox(height: 32),
-                      Text(attributes[i]),
-                      Text(values[i]),
+                      Text(
+                        attributes[i],
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: colorScheme.onPrimaryContainer,
+                        ),
+                      ),
+                      Text(
+                        values[i],
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          color: colorScheme.onPrimaryContainer,
+                        ),
+                      ),
                     ],
                   ),
                 ),
