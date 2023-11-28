4cd202640f86a1740825b18e48b8ce0e8ad2f06b
mgr <mgr@lmis.de>
2023-11-28 11:23:05 +0100

    review: moved class into its own file

---
+++ frontend/app/lib/dtos/action_dto.dart
@@ -0,0 +1,37 @@
+import "package:aw40_hub_frontend/models/diagnosis_model.dart";
+import "package:json_annotation/json_annotation.dart";
+
+part "action_dto.g.dart";
+
+@JsonSerializable()
+class ActionDto {
+  ActionDto(
+    this.id,
+    this.instruction,
+    this.actionType,
+    this.dataType,
+    this.component,
+  );
+
+  factory ActionDto.fromJson(Map<String, dynamic> json) {
+    return _$ActionDtoFromJson(json);
+  }
+
+  ActionModel toModel() {
+    return ActionModel(
+      id,
+      instruction,
+      actionType,
+      dataType,
+      component,
+    );
+  }
+
+  String? id;
+  String instruction;
+  @JsonKey(name: "action_type")
+  dynamic actionType;
+  @JsonKey(name: "data_type")
+  dynamic dataType;
+  dynamic component;
+}
