3a470880d4bca4f76a8d7fd6b864a8bc1cd58d3c
mgr <mgr@lmis.de>
2023-11-29 10:30:04 +0100

    review: added action model dto test

--- frontend/app/test/dto_test.dart
+++ frontend/app/test/dto_test.dart
@@ -1,5 +1,6 @@
 import "package:aw40_hub_frontend/dtos/action_dto.dart";
 import "package:aw40_hub_frontend/dtos/dtos.dart";
+import "package:aw40_hub_frontend/models/action_model.dart";
 import "package:aw40_hub_frontend/models/models.dart";
 import "package:aw40_hub_frontend/utils/utils.dart";
 import "package:flutter_test/flutter_test.dart";
@@ -600,6 +601,125 @@ void main() {
     });
     test("correctly assigns milage", () {
       expect(json["milage"], milage);
+    });
+  });
+  group("ActionDto primary constructor", () {
+    const String id = "some_id";
+    const String instruction = "some_customer_id";
+    const String actionType = "some_action_type";
+    const String dataType = "some_data_type";
+    const String component = "some_component";
+    final ActionDto actionDto = ActionDto(
+      id,
+      instruction,
+      actionType,
+      dataType,
+      component,
+    );
+    test("correctly assigns id", () {
+      expect(actionDto.id, id);
+    });
+    test("correctly assigns instruction", () {
+      expect(actionDto.instruction, instruction);
+    });
+    test("correctly assigns actionType", () {
+      expect(actionDto.actionType, actionType);
+    });
+    test("correctly assigns dataType", () {
+      expect(actionDto.dataType, dataType);
+    });
+    test("correctly assigns component", () {
+      expect(actionDto.component, component);
+    });
+  });
+  group("ActionDto fromJson constructor", () {
+    const String id = "some_id";
+    const String instruction = "some_customer_id";
+    const String actionType = "some_action_type";
+    const String dataType = "some_data_type";
+    const String component = "some_component";
+    final Map<String, dynamic> json = <String, dynamic>{
+      "id": id,
+      "instruction": instruction,
+      "actionType": actionType,
+      "dataType": dataType,
+      "component": component,
+    };
+    final ActionDto actionDto = ActionDto.fromJson(json);
+    test("correctly assigns id", () {
+      expect(actionDto.id, id);
+    });
+    test("correctly assigns instruction", () {
+      expect(actionDto.instruction, instruction);
+    });
+    test("correctly assigns actionType", () {
+      expect(actionDto.actionType, actionType);
+    });
+    test("correctly assigns dataType", () {
+      expect(actionDto.dataType, dataType);
+    });
+    test("correctly assigns component", () {
+      expect(actionDto.component, component);
+    });
+  });
+  group("ActionDto toJson method", () {
+    const String id = "some_id";
+    const String instruction = "some_customer_id";
+    const String actionType = "some_action_type";
+    const String dataType = "some_data_type";
+    const String component = "some_component";
+    final ActionDto actionDto = ActionDto(
+      id,
+      instruction,
+      actionType,
+      dataType,
+      component,
+    );
+    final Map<String, dynamic> json = actionDto.toJson();
+    test("correctly assigns id", () {
+      expect(json["id"], id);
+    });
+    test("correctly assigns instruction", () {
+      expect(json["instruction"], instruction);
+    });
+    test("correctly assigns actionType", () {
+      expect(json["actionType"], actionType);
+    });
+    test("correctly assigns occasion", () {
+      expect(json["dataType"], dataType);
+    });
+    test("correctly assigns component", () {
+      expect(json["component"], component);
+    });
+  });
+  group("ActionDto toModel method", () {
+    const String id = "some_id";
+    const String instruction = "some_customer_id";
+    const String actionType = "some_action_type";
+    const String dataType = "some_data_type";
+    const String component = "some_component";
+    final ActionDto actionDto = ActionDto(
+      id,
+      instruction,
+      actionType,
+      dataType,
+      component,
+    );
+    final ActionModel actionModel = actionDto.toModel();
+    test("correctly assigns id", () {
+      expect(actionModel.id, id);
+    });
+    test("correctly assigns instruction", () {
+      expect(actionModel.instruction, instruction);
+    });
+    test("correctly assigns actionType", () {
+      expect(actionModel.actionType, actionType);
+    });
+    test("correctly assigns dataType", () {
+      expect(actionModel.dataType, dataType);
+    });
+    test("correctly assigns component", () {
+      expect(actionModel.component, component);
     });
   });
 }
