ae4f211103ba1d84f160d37d55c67a4603a02cab
sply88 <sebastian.pauly@tu-dortmund.de>
2024-01-17 16:09:39 +0100

    Update test_minio.py
    New tests for failed api key authentication

--- api/tests/routers/test_minio.py
+++ api/tests/routers/test_minio.py
@@ -1,5 +1,9 @@
 import uuid
+import pytest
 from httpx import Client
+from api.routers import minio
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 # Test run again the api container. Use the api key configured for the api's
 # minio router in dev.env
@@ -48,3 +52,46 @@ def test_download_link():
     response = client.get(link)
     assert response.status_code == 200
     assert response.text == testval2
+
+
+test_app = FastAPI()
+test_app.include_router(minio.router)
+
+# API keyauth needs to be configured for the module under test
+test_api_key = "valid key"
+minio.api_key_auth.valid_key = test_api_key
+
+
+@pytest.mark.parametrize(
+    "route", minio.router.routes, ids=lambda r: r.name
+)
+def test_missing_api_key(route):
+    """
+    Endpoints should not be accessible, if no api key is passed.
+    """
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    path = route.path.replace("{bucket_name}", "any-bucket")
+    method = next(iter(route.methods))
+    test_client = TestClient(test_app)
+    response = test_client.request(method=method, url=path)
+    assert response.status_code == 403
+    assert list(response.json().keys()) == ["detail"], \
+        "No data but exception details expected in response body."
+
+
+@pytest.mark.parametrize(
+    "route", minio.router.routes, ids=lambda r: r.name
+)
+def test_invalid_api_key(route):
+    """
+    Endpoints should not be accessible, if invalid api key is passed.
+    """
+    assert len(route.methods) == 1, "Test assumes one method per route."
+    path = route.path.replace("{bucket_name}", "any-bucket")
+    method = next(iter(route.methods))
+    test_client = TestClient(test_app)
+    test_client.headers["x-api-key"] = test_api_key[1:]
+    response = test_client.request(method=method, url=path)
+    assert response.status_code == 401
+    assert list(response.json().keys()) == ["detail"], \
+        "No data but exception details expected in response body."
