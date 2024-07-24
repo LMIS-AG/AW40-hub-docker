e6ff565aaa357f00fcbefec66593799ff3ed823a
Iscia Berger <iscianorina@gmail.com>
2024-07-24 11:49:25 +0200

    Removed unused import

--- frontend/app/lib/views/case_detail_view.dart
+++ frontend/app/lib/views/case_detail_view.dart
@@ -1,5 +1,4 @@
 import "dart:async";
-import "package:aw40_hub_frontend/components/dataset_upload_area.dart";
 import "package:aw40_hub_frontend/components/dataset_upload_case_view.dart";
 import "package:aw40_hub_frontend/dialogs/update_case_dialog.dart";
 import "package:aw40_hub_frontend/dtos/case_update_dto.dart";
@@ -12,7 +11,6 @@ import "package:aw40_hub_frontend/models/timeseries_data_model.dart";
 import "package:aw40_hub_frontend/providers/auth_provider.dart";
 import "package:aw40_hub_frontend/providers/case_provider.dart";
 import "package:aw40_hub_frontend/providers/diagnosis_provider.dart";
-import "package:aw40_hub_frontend/utils/enums.dart";
 import "package:aw40_hub_frontend/utils/extensions.dart";
 import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
