584e2be2d5c5258069bf2afeaadb9c04b2f62b9f
Iscia Berger <iscianorina@gmail.com>
2024-07-25 16:25:08 +0200

    VehicleScreen

--- frontend/app/lib/screens/vehicles_screen.dart
+++ frontend/app/lib/screens/vehicles_screen.dart
@@ -1,18 +1,8 @@
-import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
 
 class VehiclesScreen extends StatelessWidget {
   const VehiclesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(tr("vehicles.title")),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => const VehiclesScreen();
 }
