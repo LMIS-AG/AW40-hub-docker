7213b88638a620dad4132afe6520fe561b1aa98c
mgr <mgr@lmis.de>
2024-08-07 15:11:18 +0200

    minor change

--- frontend/app/lib/data_sources/customer_data_table_source.dart
+++ frontend/app/lib/data_sources/customer_data_table_source.dart
@@ -1,4 +1,4 @@
-import 'package:aw40_hub_frontend/models/customer_model.dart';
+import "package:aw40_hub_frontend/models/customer_model.dart";
 import "package:flutter/material.dart";
 
 class CustomerDataTableSource extends DataTableSource {
