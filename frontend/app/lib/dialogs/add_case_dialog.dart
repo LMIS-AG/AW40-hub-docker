e3365b4c6b462914520c8c80cc5f6dba7525213d
mgr <mgr@lmis.de>
2024-08-20 12:47:49 +0200

    adjusted tooltip text

--- frontend/app/lib/dialogs/add_case_dialog.dart
+++ frontend/app/lib/dialogs/add_case_dialog.dart
@@ -301,8 +301,8 @@ class AddDialogForm extends StatelessWidget {
                   const SizedBox(width: 16),
                   Tooltip(
                     message: tr(
-                      "cases.addCaseDialog.customerTooltip",
-                    ), // TODO adjust
+                      "cases.addCaseDialog.createNewCustomerTooltip",
+                    ),
                     child: IconButton(
                       icon: const Icon(Icons.person_add),
                       onPressed: () {
