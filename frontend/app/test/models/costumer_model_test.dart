2f4ec90616cb943e50e9c02fed772829ff655fce
mgr <mgr@lmis.de>
2024-09-17 15:01:44 +0200

    extended test

--- frontend/app/test/models/costumer_model_test.dart
+++ frontend/app/test/models/costumer_model_test.dart
@@ -6,9 +6,24 @@ void main() {
     const id = "some_id";
     const firstname = "some_firstname";
     const lastname = "some_lastname";
+    const email = "some_email";
+    const phone = "some_phone";
+    const street = "some_street";
+    const housenumber = "some_housenumber";
+    const postcode = "some_postcode";
+    const city = "some_city";
 
-    final costumerModel =
-        CustomerModel(id: id, firstname: firstname, lastname: lastname);
+    final costumerModel = CustomerModel(
+      id: id,
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      phone: phone,
+      street: street,
+      housenumber: housenumber,
+      postcode: postcode,
+      city: city,
+    );
     test("correctly assigns id", () {
       expect(costumerModel.id, id);
     });
@@ -17,6 +32,24 @@ void main() {
     });
     test("correctly assigns lastname", () {
       expect(costumerModel.lastname, lastname);
+    });
+    test("correctly assigns email", () {
+      expect(costumerModel.email, email);
+    });
+    test("correctly assigns phone", () {
+      expect(costumerModel.phone, phone);
+    });
+    test("correctly assigns street", () {
+      expect(costumerModel.street, street);
+    });
+    test("correctly assigns housenumber", () {
+      expect(costumerModel.housenumber, housenumber);
+    });
+    test("correctly assigns postcode", () {
+      expect(costumerModel.postcode, postcode);
+    });
+    test("correctly assigns city", () {
+      expect(costumerModel.city, city);
     });
   });
 }
