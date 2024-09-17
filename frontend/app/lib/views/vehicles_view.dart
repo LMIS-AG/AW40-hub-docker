9b0711e1afd5d2e94e1134c5e0bd43e906391a06
mgr <mgr@lmis.de>
2024-09-17 16:36:14 +0200

    resolved todo

--- frontend/app/lib/views/vehicles_view.dart
+++ frontend/app/lib/views/vehicles_view.dart
@@ -122,10 +122,8 @@ class DesktopVehiclesViewState extends State<DesktopVehiclesView> {
             if (value == null) return const SizedBox.shrink();
             return Expanded(
               flex: 2,
-              // TODO check not null currentVehiclesIndexNotifier.value
               child: VehicleDetailView(
-                vehicleModel:
-                    widget.vehicleModel[currentVehiclesIndexNotifier.value!],
+                vehicleModel: widget.vehicleModel[value],
                 onClose: () => currentVehiclesIndexNotifier.value = null,
               ),
             );
